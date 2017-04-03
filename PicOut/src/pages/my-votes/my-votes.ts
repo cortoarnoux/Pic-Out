ce18ccb2281f8245da1eac9d5cb4e35db8d5439d
HugoMaffre <hugomaffre@yahoo.fr>
2017-04-03 09:09:56 +0200

    Page My Votes

--- PicOut/src/pages/my-votes/my-votes.ts
+++ PicOut/src/pages/my-votes/my-votes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AccueilPage } from '../accueil/accueil';
 
 /*
   Generated class for the MyVotes page.
@@ -13,10 +14,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MyVotesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public nav: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyVotesPage');
   }
+
+    public moveToHome() {
+      this.nav.push(AccueilPage);
+    }
+
+
+    votes: Array<any> = [
+        {name: "Vote 1",
+         date: "10/03/2017"},
+        {name: "Vote 2",
+         date: "07/03/2017"},
+        {name: "Vote 3",
+         date: "15/10/2017"},
+        {name: "Vote 4",
+         date: "24/03/2017"},
+        {name: "Vote 5",
+         date: "10/08/2017"}
+    ];
+
 
 }
