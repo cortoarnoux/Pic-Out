68f3b17240fbfc7244522517528c792498127b4d
HugoMaffre <hugomaffre@yahoo.fr>
2017-04-08 16:41:01 +0200

    Ajout de la page de modification de compte

--- PicOut/src/pages/my-account/my-account.ts
+++ PicOut/src/pages/my-account/my-account.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Http} from '@angular/http';
+import { CreateVotePage } from '../create-vote/create-vote';
+import { FriendsPage } from '../friends/friends';
+import { MyVotesPage } from '../my-votes/my-votes';
+import { LoginPage } from '../login/login';
+import { AccueilPage } from '../accueil/accueil';
+import firebase from 'firebase';
+import { Storage } from '@ionic/storage';
+import { CurrentUserService } from '../../providers/data/currentuser-service';
 
 /*
   Generated class for the MyAccount page.
@@ -13,10 +22,46 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MyAccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  //Récupération de l'objet user actuel de firebase
+  public currentUser = firebase.auth().currentUser;
 
+  public thisUser: any;
+
+  public constructor(
+    public nav: NavController,
+    private platform: Platform,
+    private http: Http,
+    private currentUserService: CurrentUserService
+  ) {}
+
+  // Corto : Au chargement de la page
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyAccountPage');
+    // Corto : Récupération des données de l'objet user de l'utilisateur actuel
+    this.currentUserService.getCurrentUser(this.currentUser.uid).on('value', (data) => {
+      this.thisUser = data.val();
+      // Corto : Si l'objet n'existe pas dans la base de donnée et donc ne peut être récupéré, création de l'objet
+      console.log(this.thisUser);
+      if(this.thisUser == null){
+        // Corto : Appel de la fonction writeUserData avec en paramètre les infos de l'objet User
+        this.currentUserService.setCurrentUser(this.currentUser.uid, this.currentUser.email);
+      }
+    });
+  }
+
+
+
+  // récupération des infos de l'utilisateur
+  user = firebase.auth().currentUser;
+  surname = this.user.email.replace(/@.*$/,"");
+  showSurname = "Bonjour " + this.surname + " !";
+  showEmail = this.user.email;
+  
+
+
+
+  //retour à l'accueil
+  public moveToMyHome() {
+    this.nav.push(AccueilPage);
   }
 
 }
