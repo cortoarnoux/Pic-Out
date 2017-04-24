155b0aa9f59054f1d399cba56e4cd3a44fe89570
White-PC\White <corto.arnoux@laposte.net>
2017-04-24 10:40:23 +0200

    - Debug username s'affiche à nouveau sur la nouvelle page modification de compte

--- PicOut/src/pages/edit-account/edit-account.ts
+++ PicOut/src/pages/edit-account/edit-account.ts
@@ -42,7 +42,7 @@ export class EditAccountPage {
       this.thisUser = data.val();
       this.currentUserEmail = this.thisUser.email;
       if(this.thisUser.username != null){
-        this.currentUserUsername = this.thisUser.userame;
+        this.currentUserUsername = this.thisUser.username;
       }
       this.currentUserFirstName = this.thisUser.firstname == null ? "Votre prénom" : this.thisUser.firstname;
       this.currentUserLastName = this.thisUser.lastname == null ? "Votre nom" : this.thisUser.lastname;
